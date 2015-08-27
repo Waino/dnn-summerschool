6e1f3aec2ce3def61a72ae9ca27d82a712c06688
Seppo Enarvi <seppo.git@marjaniemi.com>
2015-08-27 14:50:37 +0300

    New model file name.

--- rnnlm/train_lm.py
+++ rnnlm/train_lm.py
@@ -24,7 +24,7 @@ def main(job_id, params):
 
 if __name__ == '__main__':
     main(0, {
-        'model': ['model.npz'],
+        'model': ['model-train10k-lstm.npz'],
         'dim_word': [256],
         'dim': [1024],
         'n-words': [30000], 
